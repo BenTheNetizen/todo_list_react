5beb2cdfb3b4c58a5bd28478345d07f7e2a4a276
noviceprogrammer <benisin5@gmail.com>
2021-04-02 19:53:30 -0400

    removed warning message

--- src/App.js
+++ src/App.js
@@ -88,7 +88,6 @@ function App(props) {
       </div>
       <h2 id="list-heading"> {headingText} </h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
